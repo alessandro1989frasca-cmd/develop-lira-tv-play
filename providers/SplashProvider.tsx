0aded85ca3740435399b544a64572ebb7cee6cd1
alexander <alessandro1989frasca@gmail.com>
2026-04-01 16:34:54 +0200

    delete

--- providers/SplashProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { useState, useMemo } from 'react';
-import createContextHook from '@nkzw/create-context-hook';
-
-export const [SplashProvider, useSplash] = createContextHook(() => {
-  const [isSplashVisible, setIsSplashVisible] = useState(true);
-  return useMemo(() => ({ isSplashVisible, setIsSplashVisible }), [isSplashVisible]);
-});
